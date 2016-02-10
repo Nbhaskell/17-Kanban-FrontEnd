25e98a6bce205951b281e6b057fa5eb4716c660e
Nathan Haskell <Nathan Haskell>
2016-02-10 11:41:17 -0800

    finished list in kanban app. need to continue on cards next

---
+++ Kanban/js/app.js
@@ -0,0 +1,27 @@
+﻿// The Angular Module
+angular.module('kanban', ['ngResource']);
+
+angular.module('kanban').value('apiUrl', 'http://localhost:63325/api');
+
+angular.module('kanban').controller('IndexController', function ($scope, $resource, apiUrl) {
+
+    var ListResource = $resource(apiUrl + '/lists/:listId', { listId: '@id' });
+
+    function activate() {
+        var lists = ListResource.query();
+
+        $scope.lists = lists;
+    }
+
+    $scope.newList = {};
+
+    $scope.addList = function () {
+        ListResource.save($scope.newList, function () {
+            alert("Save Successful'");
+            activate();
+        });
+    };
+
+    activate();
+});
+
